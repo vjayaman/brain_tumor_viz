62062474f9fa8bc99d61e920744c1b1272ba5a2c
vjayaman <vjayaman314@gmail.com>
2026-03-07 02:07:05 -0600

    t-SNE exploratory work, will see how it runs with this plot in layout

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
  - *app.R* is for preliminary layout/design testing, reads *brain_tumor_dataset.csv* and *functions.R*
 
  - *linked_chart_eg1.R* is standalone example of linked charts (first attempt before app) - less options with this approach
+
+ - *tsne_exploratory.R* is a standalone example of a plot we could use to visualize the dimensionality reduction performed by t-SNE. This could be a user option of t-SNE or PCA, to see if there are any potential cluster groups (this could be pre-run for several perplexity values and the user could flip through them). We could add this as a secondary tab beside the age-gender plot.
---
+++ tsne_exploratory.R
@@ -0,0 +1,40 @@
+library(shiny); library(shinyWidgets)
+libs <- c("tidyverse", "dplyr", "magrittr", "readr", "ggplot2", 
+          "janitor", "data.table", "DT", "rlc", "bslib", "plotly", 
+          "highcharter", "Rtsne")
+y <- suppressPackageStartupMessages(lapply(libs, require, character.only = TRUE))
+
+source("functions.R")
+
+# ----- tSNE -------------------------------------------------------------------
+set.seed(42)
+# ref: https://www.geeksforgeeks.org/r-language/t-distributed-stochastic-neighbor-embedding-t-sne-using-r/
+colors <- c("#E6194B", "#3CB44B", "#FFE119", "#4363D8", "#F58231", "#911EB4", "#46F0F0", "#F032E6", "#BCF60C", "#FABEBE")
+
+br <- loadData()
+br_red <- br %>% select(Age, Tumor_Size, Survival_Rate, Tumor_Growth_Rate, Location)
+# br_red <- br %>% select(-Patient_ID, -Gender)
+
+char_cols <- sapply(br_red, class) %>% grep("character", ., , value = TRUE) %>% names()
+br_mat <- br_red %>% 
+  mutate(across(all_of(char_cols), ~ as.numeric(as.factor(.x)))) %>% 
+  as.matrix() %>% unique() %>% 
+  scale()
+
+# iter = 5000, dims = 3, perp = 50 gives distinct clusters
+n_iter <- 5000
+n_dims <- 3
+n_perp <- 50
+br_res <- br_mat %>% unique() %>% 
+  Rtsne(., dims = n_dims, perplexity = n_perp, verbose = TRUE, 
+        max_iter = n_iter, normalize = TRUE, num_threads = 0)
+# saveRDS(br_res, paste0("stand_nogen_dims-", n_dims, "_perp-", n_perp, "_iter-", n_iter, ".Rds"))
+
+br_dat <- data.frame(br_res$Y)
+br_dat$ColorBy <- br$Stage[!duplicated(br_mat)]
+colnames(br_dat) <- c("Dim1", "Dim2", "Dim3", "ColorBy")
+# br_dat$ColorBy <- findInterval(br_dat$ColorBy, c(0.5,1,1.5,2,2.5,3)) %>% as.character()
+
+plot_ly(br_dat, x = ~Dim1, y = ~Dim2, z = ~Dim3,
+        type = "scatter3d", mode = "markers", colors = colors, 
+        marker = list(size = 6), color = ~ColorBy)
